2902673d377345913b0b8991c1713ad9ebfac523
Vasudev08 <vasu14devagarwal@gmail.com>
2023-12-13 16:00:57 -0600

    added tictactoe created in C# WPF

---
+++ Learning_C/Learning_CSharp/Cclasses.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp
+{
+    /// <summary>
+    /// Contains code of videos I have watched for C#
+    /// </summary>
+    public class Cclasses
+    {
+            
+
+
+
+            //variales are pecies of information
+            byte myByte = 12;
+            //byte is represent of 8 bit 0 - 255
+            // 8 - bit variable
+            char myChar = 'c';
+            // single letter
+
+            short myShort = 65; // 16 bits --> comment code
+
+
+
+            int myInt = 2400000; // bits
+            uint myUint = 4; // can store larger number as it doesn't contain negative number
+            float myFloat = 1.2235f;  // 32 bits
+            double myDouble = 1.2345; // 64 bits
+            decimal myDecimal = (decimal)1.444444;  // 128 bits
+
+            string myString = "Hello World";
+
+            // bool myBool = false;
+            bool myBool = true;
+
+            // ---> this is a comment
+            /*
+             Multi Line Comment
+             */
+
+            // 2. Ending lines and statements
+
+            byte myBytes = 12;
+            // '=' -> operator
+
+
+            // 3. Accessibilty levels
+            // private
+
+
+            // 4. different ways to declare variables (members v/s properties)
+            //private byte mMyVariable;
+            // private string mCreditCardNumber;
+
+
+            // 5. Reading text from user input
+            
+
+            // 6. Parsing
+            
+
+
+    }
+}
