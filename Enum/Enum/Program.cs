af473c1f1c8e948b630c7d3b744feab438d8000f
Vasudev08 <vasu14devagarwal@gmail.com>
2023-12-15 12:19:12 -0600

    Learned about Generic Class and Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enums 
+{
+
+    // custom -defined constant values for custom use
+    public enum MyEnums
+    {
+        Myvalue1,
+        Myvalue2
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+
+            // 1. What are enum's ?
+            int i = 1;
+            var a = MyEnums.Myvalue1;
+
+            // 2. Different base types
+            var myInt = (int)MyEnums.Myvalue2;
+
+
+            // 3. Where to use them ?
+
+
+            // 4. Casting to/from int / type
+
+
+            // 5. Parsing from string name
+
+
+            // 6. Enumerating the enum
+
+
+            // 7. Flags
+        }
+    }
+}
---
+++ GenericClass/GenericClass/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    public class MyGenericClass<T>
+    { 
+    public T MyNumber { get; set; }
+    }
+
+
+
+    public class MyList<T>
+    {
+        private List<T> mNumbers = new List<T>();
+
+        public void AddNumber(T number)
+        {
+            mNumbers.Add(number);
+
+        }
+        public T GetNumber(int i)
+        {
+            return mNumbers[i];
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new MyGenericClass<int>();
+            var c2 = new MyGenericClass<bool>();
+            // why we need genericClass because what if we need to do same operation to mutiple data-type classes
+            // GenericClass doesn't have a specific type as it's determined during compile-time
+
+            var numbers = new MyList<int>();
+            numbers.AddNumber(10);
+            numbers.AddNumber(20);
+            numbers.AddNumber(30);
+            numbers.AddNumber(40);
+            numbers.AddNumber(50);
+
+            var list2 = new MyList<string>();
+            list2.AddNumber("1");
+            list2.AddNumber("4");
+
+
+            Console.WriteLine(numbers.GetNumber(3));
+
+        }
+    }
+
+
+
+    
+}
