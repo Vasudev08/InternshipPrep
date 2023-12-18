eb8cec90854ab0640bbb85980ff6191ce9b060bd
Vasudev08 <vasu14devagarwal@gmail.com>
2023-12-18 14:14:59 -0600

    wpf basics and learning about abstract class

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace AbstractClass
+{
+
+    public abstract class Car
+    {
+
+        #region Public Properties
+        // virtual methods allow the dervied class to override the function implementation
+
+
+        public abstract int WheelDiameterInches { get; }
+
+        /// <summary>
+        /// Indicates if the handbrake is released
+        /// </summary>
+        public bool HandBrakeRelased { get; set; } = false;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Turns the car radio on
+        /// </summary>
+        public virtual void RadioOn()
+        {
+            Console.WriteLine("Turned the radio on");
+        }
+
+        /// <summary>
+        /// Attempts to release the cars handbrake
+        /// </summary>
+        public virtual void ReleaseHandbrake()
+        {
+            HandBrakeRelased = true;
+            Console.WriteLine("Successfully released the handbrake");
+        }
+
+
+        /// <summary>
+        /// Goes on the gas to drive the car
+        /// </summary>
+        public virtual void Drive()
+        {
+            // Release the handbrake
+            ReleaseHandbrake();
+            if (HandBrakeRelased)
+            {
+                Console.WriteLine("Pressed the gas peddle");
+            }
+            else
+            {
+                Console.WriteLine("Cannot move... hardbrake jammed");
+            }
+
+        }
+        // Standard methods
+        public float GearboxOutputSpeedRpm()
+        {
+            return 1000;
+        }
+        public float CalculateSpeedMph()
+        {
+            // if handbrake is on, we are not moving
+            if  (!HandBrakeRelased)
+                return 0;
+
+            var inchesPerRev = (float)(Math.PI * WheelDiameterInches);
+            var inchesPerMinute = inchesPerRev * GearboxOutputSpeedRpm();
+            var mph = inchesPerMinute * 60 * 0.0000157828;
+
+            return (float)mph;
+        }
+
+
+
+        #endregion
+    }
+
+    public class BudgetCar : Car
+    {
+        public override int WheelDiameterInches => 16;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+        }
+    }
+
+
+}
---
+++ WpfBasics/WpfBasics/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The Description is: {this.DescriptionText.Text}");
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked = this.LatheCheckBox.IsChecked = this.PurchaseCheckBox.IsChecked =
+                this.LatheCheckBox.IsChecked = this.DrillCheckBox.IsChecked = this.FoldCheckBox.IsChecked = this.RollCheckBox.IsChecked = this.SawCheckBox.IsChecked = false;
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += (string)((CheckBox)sender).Content;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.NoteText.Text = (string)((ComboBox)sender).SelectedValue;
+        }
+    }
+}
