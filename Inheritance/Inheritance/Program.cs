492526bc7183d644b00d12417e62b7ea7efb95b2
Vasudev08 <vasu14devagarwal@gmail.com>
2023-12-18 14:16:47 -0600

    learned inheritance and modified modernDesign

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Program
+{
+    class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            //1. Base Class - Derived Class
+            //2. Constructor
+            //3. Casting
+            //4. Sealed
+            //5. Inheriting an inherited class
+
+
+            var car = new Car();
+
+            var Ferrari = new Ferrari();
+        }
+    }
+
+
+
+    public class Car
+    {
+        public int WheelSize { get; set; }
+        public int NumberofDoors { get; set; }
+
+        public Car()
+        {
+        }
+    }
+
+
+    public class Ferrari : Car
+    {
+        public Ferrari(int wheelSize)
+        {
+            WheelSize = wheelSize;
+
+        }
+
+    }
+
+    // NO heritating multiple classes, we can only one class inheritaned
+}
