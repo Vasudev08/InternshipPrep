72fa72bfe11ece3352fb00301205a6aed1ad0f50
Vasudev08 <vasu14devagarwal@gmail.com>
2023-12-31 18:49:42 -0600

    Started new project from youtube called YouTube Viewers

---
+++ WpfTreeView/WpfTreeView/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+
+        #region On Loaded
+
+        /// <summary>
+        /// When the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get every logical drive on the machine
+            foreach (var drive in Directory. GetLogicalDrives()) 
+            {
+                // Create a new item for it
+                var item = new TreeViewItem();
+
+                // Set the header and path
+                item.Header = drive;
+                item.Tag = drive;
+
+                // Add a dummy item
+                item.Items.Add(null);
+
+                // Listen out for item being expanded
+                item.Expanded += Folder_Expanded;
+
+                FolderView.Items.Add(item);
+
+            }
+
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            // If the item only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+
+            }
+
+            // Clear dummy data
+            item.Items.Clear();
+
+            // Get folder name
+            var fullPath = (string)item.Tag;
+
+            // Create a blank list for directories
+            var directories = new List<string>();
+
+            // Try and get directioories from the folder
+            // Ignoring any issues doing so
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+            catch { }
+
+
+                
+                
+
+        }
+
+        #endregion
+    }
+}
---
+++ YouTubeViewer/YouTubeViewer.WPF/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace YouTubeViewer.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
