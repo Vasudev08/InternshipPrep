57511009c527819b9be488f530d83361abf6d9f9
Vasudev08 <vasu14devagarwal@gmail.com>
2024-01-16 14:12:20 -0600

    function overloading, inheritance, fields and properties with setters and getters

---
+++ CSharpPractice/CSharpPractice/Classes/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.Classes
+{
+    public class BankAccount
+    {
+
+        public BankAccount()
+        {
+            Balance = 100;
+        }
+
+        public BankAccount(double initialBalance)
+        {
+            Balance = initialBalance;
+        }
+        // Properties --> adds security of viewing it 
+        private double balance; // variable for holding account balance
+        public double Balance
+        {
+            get 
+            { 
+                if (balance < 1000000)
+                    return balance;
+                return 100000000;
+            }
+            protected set
+            { 
+                if (value > 0)
+                    balance = value;
+                else
+                    balance = 0;
+            }
+        }
+
+
+
+        public double AddtoBalance(double balanceToBeAdded)
+        {
+            Balance += balanceToBeAdded;
+            return Balance;
+        }
+    }
+
+    public class ChildBankAccount : BankAccount
+    {
+        public ChildBankAccount()
+        {
+            Balance = 10;
+        }
+    }
+}
---
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System;
+using CSharpPractice.Classes;
+
+namespace CSharpPractice
+
+{
+    class Program
+    {
+        static double numberTwo = 12.34;
+        static void Main(string[] args)
+        {
+
+            double[] numbers = [1, 2, 4, 42, 4256];
+            var result = SimpleMath.Add(numbers);
+            Console.WriteLine(result);
+
+
+            BankAccount bankAccount = new BankAccount(1000);//
+            bankAccount.AddtoBalance(100);
+            Console.WriteLine(bankAccount.Balance);
+
+
+            ChildBankAccount childBankAccount = new ChildBankAccount();
+            childBankAccount.AddtoBalance(10);
+            Console.WriteLine(childBankAccount.Balance);
+
+            Console.ReadLine();
+            //bankAccount.AddtoBalance(numberTwo); // Bankaccount class isn't an static method so we have to declare or instnation it
+        }
+    }
+
+    class SimpleMath
+    {
+
+        // static methods are methods which can be used without instantiating a class
+        // for using these methods, 
+
+        // Public, Private, Protected
+        // Method Overloading
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        // method overloading - same method name but difference paramters
+        public static double Add(double[] numbers )
+        {
+            double result = 0;
+            foreach(double d in numbers)
+            {
+                result += d;
+            }
+
+            return result;
+
+        }
+    }
+
+
+}
