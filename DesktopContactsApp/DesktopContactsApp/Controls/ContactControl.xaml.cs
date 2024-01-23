3fd154cba741ebbba13b92536453bba3e89e1bc1
Vasudev08 <vasu14devagarwal@gmail.com>
2024-01-23 11:45:42 -0600

    custom contorl

---
+++ DesktopContactsApp/DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DesktopContactsApp.Classes;
+
+namespace DesktopContactsApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        private Contact contact;
+        public Contact Contact
+        {
+            get
+            {
+                return contact;
+            }
+            set
+            {
+                contact = value;
+                nameTextBlock.Text = contact.Name;
+                PhoneTextBlock.Text = contact.Phone;
+                emailTextBlock.Text = contact.Email;
+
+            }
+        }
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DesktopContactsApp/DesktopContactsApp/obj/Debug/net8.0-windows/DesktopContactsApp_2nlsdacc_wpftmp.AssemblyInfo.cs
+++ DesktopContactsApp/DesktopContactsApp/obj/Debug/net8.0-windows/DesktopContactsApp_2nlsdacc_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d8f80e4fc224107440bce127543f965075086f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
