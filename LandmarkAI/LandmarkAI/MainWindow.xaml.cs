2cdbbfc0c016c13f736caa10e00d09c06fd95301
Vasudev08 <vasu14devagarwal@gmail.com>
2024-01-25 14:45:02 -0600

    Created customVistion which recognizes the uploaded images and tells what it is

--- LandmarkAI/LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/LandmarkAI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,7 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LandmarkAI.Classes;
 using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace LandmarkAI
 {
@@ -25,12 +29,45 @@ namespace LandmarkAI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.png; *.jpg)|*.png; *.jpg;*jpeg|All files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
             if (dialog.ShowDialog() == true)
             {
                 string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
 
             }
 
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/cda7532f-c67a-4fb3-8e21-0205a1248086/classify/iterations/LandMarkPrediction/image";
+            string prediction_key = "fce5859dace34817ab70c9914c8be236";
+            string content_type = "application/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", prediction_key);
+
+                using(var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(content_type);
+                    var response = await client.PostAsync(url, content);
+
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+
+                    List<Prediction> predictions = (JsonConvert.DeserializeObject<CustomVision>(responseString)).predictions; 
+                    predictionsListView.ItemsSource = predictions;
+
+                    
+                }
+            }
         }
     }
 }
