9e83807e910a7dfdbd12797a24c75d4ef4d6122a
Vasudev08 <vasu14devagarwal@gmail.com>
2024-01-28 12:44:09 -0600

    Binding View and ViewModel using IpropertyChange

---
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get 
+            { 
+                return query;
+            }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentCondition currentCondition;
+
+        public CurrentCondition CurrentCondition
+        {
+            get { return currentCondition; }
+            set 
+            { 
+                currentCondition = value;
+                OnPropertyChanged("CurrentConditions");
+            
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            { 
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName) 
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
